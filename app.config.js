b385bdfc8c896fd83e9fb5f9d8a5903b717e986e
Matheus Peluchi Rocha <matheus.rocha@softplan.com.br>
2025-07-30 16:42:08 -0400

    Atualizar versao

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ const appConfig = {
     "name": "Todo",
     "slug": "todo",
     "scheme": "todo",
-    "version": "1.1.3",
+    "version": "1.1.4",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
